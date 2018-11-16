5918f2ca19c2fe74a68a73e8ff2b0c66c20f929f
Leonardo Colombo <designer@coobrastur.net>
2018-11-16 14:37:37 -0200

    instalacao do generator builder

--- routes/web.php
+++ routes/web.php
@@ -80,3 +80,9 @@ Route::resource('convenios', 'ConvenioController');
 Route::resource('representantes', 'RepresentanteController');
 
 Route::resource('vendasres', 'VendasreController');
+
+Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder');
+
+Route::get('field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@fieldTemplate');
+
+Route::post('generator_builder/generate', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generate');

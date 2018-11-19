a8dfee3ee070bab8a4e5dc8155bf79793118b330
Leonardo Colombo <designer@coobrastur.net>
2018-11-19 13:57:51 -0200

    composer update

--- routes/web.php
+++ routes/web.php
@@ -84,23 +84,3 @@ Route::resource('convenios', 'ConvenioController');
 Route::resource('representantes', 'RepresentanteController');
 
 Route::resource('vendasres', 'VendasreController');
-
-Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder');
-
-Route::get('field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@fieldTemplate');
-
-Route::post('generator_builder/generate', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generate');
-
-Route::resource('permissoes', 'PermissoesController');
-
-Route::resource('permissoes', 'PermissoesController');
-
-Route::resource('permissoes', 'PermissoesController');
-
-Route::resource('permissoes', 'PermissoesController');
-
-Route::resource('permissoesEs', 'PermissoesEsController');
-
-Route::resource('permissoesEs', 'PermissoesEsController');
-
-Route::resource('permissoesEs', 'PermissoesEsController');

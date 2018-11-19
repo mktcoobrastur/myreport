eb7400c58a44b3856caecd5e0cead8cf3421196e
Leonardo Colombo <designer@coobrastur.net>
2018-11-19 15:23:13 -0200

    trabalhando data

---
+++ public/separaData.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<table class="table table-bordered">
+<thead>
+    <tr>
+      <th scope="col">Data</th>
+      <th scope="col">Atendente</th>
+      <th scope="col">Qnt</th>
+      <th scope="col">Plano</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+    $con = new mysqli("localhost", "root", "", "sistema");
+    $mes = '11';
+    // Filtro mês
+    $consulta = mysqli_query($con, "SELECT * FROM vendasdia WHERE MONTH(created_at) = $mes ORDER BY created_at DESC");
+        while($l = mysqli_fetch_object($consulta)) {
+?>
+    <tr>
+        <th><?php echo $l->created_at; ?></th>
+        <th><?php echo $l->atendente; ?></th>
+        <th><?php echo $l->qnt; ?></th>
+        <th><?php echo $l->plano; ?></th>
+    </tr>
+<?php } ?>   
+</tbody>
+</table>

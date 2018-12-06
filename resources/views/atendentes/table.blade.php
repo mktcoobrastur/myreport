2685590b02ec01b689f21684282d78a3ed053b90
Leonardo Colombo <designer@coobrastur.net>
2018-12-06 17:28:36 -0200

    commit

--- resources/views/atendentes/table.blade.php
+++ resources/views/atendentes/table.blade.php
@@ -3,7 +3,7 @@
         <tr>
             <th>Nome Atendente</th>
             <th>Representante</th>
-            <th colspan="3">Ações</th>
+            <th colspan="3" style="text-align: right;">Ações</th>
         </tr>
     </thead>
     <tbody>
@@ -13,7 +13,7 @@
             <td>{!! strtoupper($atendente->representante) !!}</td>
             <td>
                 {!! Form::open(['route' => ['atendentes.destroy', $atendente->id], 'method' => 'delete']) !!}
-                <div class='btn-group'>
+                <div class='btn-group pull-right'>
                     <a href="{!! route('atendentes.show', [$atendente->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
                     <a href="{!! route('atendentes.edit', [$atendente->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Tem certeza?')"]) !!}
@@ -22,21 +22,5 @@
             </td>
         </tr>
     @endforeach
-
-            <tr>
-            <th>&nbsp;</th>
-            <th>&nbsp;</th>
-            <th style="text-align: center;">
-                <?php
-                    $con = new mysqli("localhost", "root", "", "sistema");
-                    $consultaTotal = mysqli_query($con, "SELECT SUM(qnt_vendas) qnt_vendas FROM atendentes");
-                    $total = mysqli_fetch_array($consultaTotal);
-                    ?>
-                    Total: <?php echo $total['qnt_vendas'];
-                ?>
-            </th>
-            <th colspan="3">&nbsp;</th>
-        </tr>
-
     </tbody>
 </table>

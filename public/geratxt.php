35b779a15c956b5e29875e756c8e534cd489a383
Leonardo Colombo <designer@coobrastur.net>
2019-01-02 11:28:17 -0200

    commit

--- public/geratxt.php
+++ public/geratxt.php
@@ -4,7 +4,7 @@
     $con = new mysqli("mysql05-farm61.uni5.net", "marketingcoobr03", "i8h9p5z2", "marketingcoobr03");
     $consulta2 = mysqli_query($con, "SELECT * FROM newsdiamante ORDER BY id DESC");
     while ($l2 = mysqli_fetch_array($consulta2)) {
-        $text .= $l2['email']."; \r\n";
+        $text .= $l2['email'].";\r\n";
 
     }
 
